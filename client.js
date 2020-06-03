86d51f49fa750633b2735c47c2f0ba52937a2165
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-06-03 01:00:10 +0000

    implementing the chznges

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+const { IP, PORT } = require('./constraints');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: IP,
+    port: PORT
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  conn.setEncoding('utf8'); 
+  
+  conn.on('connect', () => {
+    console.log('Succefully connected to game server!');
+    conn.write('Name: AMA');
+  });
+  return conn
+}
+
+module.exports = { connect };
